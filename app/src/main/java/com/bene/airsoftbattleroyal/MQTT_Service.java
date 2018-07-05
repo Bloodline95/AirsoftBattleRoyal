a127870061f6dac687a90d84d8525b817c6684b1
Bloodline95 <benedict.kellermann@web.de>
2018-07-05 22:02:57 +0200

    patch

--- app/src/main/java/com/bene/airsoftbattleroyal/MQTT_Service.java
+++ app/src/main/java/com/bene/airsoftbattleroyal/MQTT_Service.java
@@ -1,12 +1,10 @@
 package com.bene.airsoftbattleroyal;
 
+import android.app.Service;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
@@ -15,56 +13,34 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.w3c.dom.Text;
 
 import java.io.UnsupportedEncodingException;
 
-public class MainActivity extends AppCompatActivity {
+abstract class MQTT_Service extends Service {
+
+    int mStartMode;
     private static final String TAG = "TAG";
+    final String serverUri = "tcp://m12.cloudmqtt.com:11111";
 
+    public MQTT_Service() {
+    }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-        //Spiel erstellen
-        final Button btn_erstellen = findViewById(R.id.button_erstellen);
-        btn_erstellen.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent erstellen_intent = new Intent(MainActivity.this, game_create.class);
-                startActivity(erstellen_intent);
-            }
-        });
-
-        //Spiel beitreten
-        final Button btn_beitreten = findViewById(R.id.button_beitreten);
-        btn_beitreten.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String username;
-                TextView nutzername = findViewById(R.id.editText);
-                username = nutzername.getText().toString();
-
-                Intent intent = new Intent(MainActivity.this, MQTT_Service.class);
-                intent.putExtra("USERNAME", username);
-                startService(intent);
-
-                Intent beitreten_intent = new Intent(MainActivity.this, game_choose.class);
-                beitreten_intent.putExtra("USERNAME", username);
-                startActivity(beitreten_intent);
-            }
-        });
+    public void onCreate(){
 
     }
 
-    void connect(String user){
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // The service is starting, due to a call to startService()
+        //Connect to Server onCreate
+        Bundle bundle = intent.getExtras();
+        String user = bundle.getString("USERNAME");
         String clientId = MqttClient.generateClientId();
         MqttAndroidClient client =
-                new MqttAndroidClient(MainActivity.this, "tcp://broker.hiveqm.com:1883",
-                        clientId);
+                new MqttAndroidClient(MQTT_Service.this, serverUri, clientId);
 
         MqttConnectOptions options = new MqttConnectOptions();
+
         options.setUserName(user);
         try{
             IMqttToken token = client.connect(options);
@@ -84,7 +60,15 @@ public class MainActivity extends AppCompatActivity {
         } catch(MqttException e){
             e.printStackTrace();
         }
+        return mStartMode;
+    }
 
+    @Override
+    public void onDestroy() {
+        // The service is no longer used and is being destroyed
+    }
+
+    public void sendMessage(String payload){
         String topic = "foo/SQL_inject";
         String payload = "0echo \"UPDATE Spieler SET USERNAME = '" + user + "'\" | mysql -uroot -proot root";
         byte[] encodedPayload = new byte[0];
